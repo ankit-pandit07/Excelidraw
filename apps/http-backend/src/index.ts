031821d1bc154fd19195c280c3cbba196cb550a7
ankit-pandit07 <panditankit865@gmail.com>
2025-10-12 13:41:54 +0530

    Added zod files

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,33 @@
 import express from "express";
 import jwt from "jsonwebtoken"
 import { middleware } from "./middleware";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {CreateRoomSchema, CreateUserSchema, CreateUserSignin} from "@repo/common/types"
  
 const app=express();
 
 app.post("/signup",(req,res)=>{
+    const data=CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+    //db call
+    res.json({
+        userId:"123"
+    })
     
 })
 app.post("/signin",(req,res)=>{
+    const data=CreateUserSignin.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
 
     const userId=1;
     const token=jwt.sign({
@@ -20,6 +39,12 @@ app.post("/signin",(req,res)=>{
     })
 })
 app.post("/room",middleware,(req,res)=>{
+    const data=CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+    }
 
 })
 
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer} from "ws";
 import  Jwt, { JwtPayload }  from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 const wss=new WebSocketServer({ port:8080 });
 
 wss.on('connection', function connection(ws,request){
@@ -12,7 +12,10 @@ wss.on('connection', function connection(ws,request){
    const queryparams=new URLSearchParams(url.split('?')[1]);
    const token=queryparams.get('token') || ""; 
    const decoded=Jwt.verify(token,JWT_SECRET)
-
+   if(typeof decoded =="string"){
+      ws.close();
+      return;
+   }
    if(!decoded || !(decoded as JwtPayload).userId){
       ws.close();
       return;
