40d351abf3cff4e027e3777a0dd52539ac4715ef
ankit-pandit07 <panditankit865@gmail.com>
2025-10-15 09:18:04 +0530

    restructure

--- apps/excelidraw-fronted/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw-fronted/app/canvas/[roomId]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { initDraw } from "@/app/draw";
 import { useEffect, useRef } from "react"
 
 
@@ -7,40 +8,14 @@ export default function Canvas(){
 
     useEffect(()=>{
         if(canvasRef.current){
-            const canvas=canvasRef.current;
-            const ctx=canvas.getContext("2d");
+           
+            initDraw(canvasRef.current);
 
-            if(!ctx){
-                return;
-            }
-            let clicked=false;
-            let startX=0;
-            let startY=0;
-
-            canvas.addEventListener("mousedown",(e)=>{
-                clicked=true
-                startX=e.clientX
-                startY=e.clientY
-            })
-            canvas.addEventListener("mouseup",(e)=>{
-                clicked=false
-                console.log(e.clientX)
-                console.log(e.clientY)
-            })
-            canvas.addEventListener("mousemove",(e)=>{
-                if(clicked){
-              const width=e.clientX-startX;
-              const height=e.clientY-startY;
-
-              ctx.clearRect(0,0,canvas.width,canvas.height)
-              ctx.strokeRect(startX,startY,width,height)
-                }
-            })
         }
     },[canvasRef])
 
     return <div>
-        <canvas ref={canvasRef} width={500} height={500}></canvas>
+        <canvas ref={canvasRef} width={2000} height={1000}></canvas>
     </div>
 
    
